8e262ec4d1d8d4cde0f3fe6fce0bd941d6eaaf41
Antoine AUBÉ <aube.antoine@protonmail.com>
2018-02-19 23:39:39 +0100

    Update the fetch-repositories script

--- scripts/update-repositories.sh
+++ scripts/update-repositories.sh
@@ -7,6 +7,8 @@ function project {
     project_name=$1
     project_url=$2
 
+    echo "Project '$project_name'..."
+
     if [ ! -d "../cloned-projects/$project_name" ]; then
 	git clone $project_url ../cloned-projects/$project_name
     else
@@ -30,3 +32,24 @@ project brainstorm https://github.com/IDSIA/brainstorm
 project pyevolve https://github.com/perone/Pyevolve
 project pytorch https://github.com/pytorch/pytorch
 project cogitare https://github.com/cogitare-ai/cogitare
+project featureforge https://github.com/machinalis/featureforge
+project metric-learn https://github.com/metric-learn/metric-learn
+project simpleai https://github.com/metric-learn/metric-learn
+project astroML https://github.com/astroML/astroML
+project Lasagne https://github.com/Lasagne/Lasagne
+project hebel https://github.com/hannes-brt/hebel
+project Chainer https://github.com/chainer/chainer
+project prophet https://github.com/facebook/prophet
+project gensim https://github.com/RaRe-Technologies/gensim
+project Surprise https://github.com/NicolasHug/Surprise
+project crab https://github.com/muricoca/crab
+project nilearn https://github.com/nilearn/nilearn
+project pyhsmm https://github.com/mattjj/pyhsmm
+project skll https://github.com/EducationalTestingService/skll
+project spearmint https://github.com/JasperSnoek/spearmint
+project deap https://github.com/deap/deap
+project mlxtend https://github.com/rasbt/mlxtend
+project tpot https://github.com/EpistasisLab/tpot
+project pgmpy https://github.com/pgmpy/pgmpy
+project milk https://github.com/luispedro/milk
+
