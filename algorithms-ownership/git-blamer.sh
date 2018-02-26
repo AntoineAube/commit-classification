bdcca57ce53b54f45c824f6c666124decd3e9b20
Antoine AUBÉ <aube.antoine@protonmail.com>
2018-02-26 15:25:06 +0100

    Start format algorithms ownership

---
+++ algorithms-ownership/git-blamer.sh
@@ -0,0 +1,8 @@
+echo "FILE_NAME,FILE_LINES,AUTHOR_NAME,AUTHOR_LINES" > $1
+
+for filename in `find . -type f -not -path "./.git/*" -not -path "./.idea/*"`
+do
+    file_lines=$(wc -l < $filename)
+    git blame --line-porcelain $filename | grep "^author " | sed -e "s/^author //" | sort | uniq -c | sort -nr | awk '{ lines = $1; $1 = ""; print $0 "," lines; }' | sed -e "s/^ //"  -e "s|^|$filename,$file_lines,|" >> $1
+
+done
